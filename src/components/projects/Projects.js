113675db7e238cb8b769096aec9bcf89996e9142
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-01-07 21:40:06 -0500

    Add multiple example projects and remove static columns

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 import Layout from "../../Layout.js";
 import { PreviewCardSmall } from ".";
 import "./Projects.scss";
-// import cubeSide from "../../resources/cube/side.JPG";
+import cubeSide from "../../resources/cube/side.JPG";
 import cubeBrick from "../../resources/cube/brick.jpg";
-// import cubeLights from "../../resources/cube/lights.jpg";
+import cubeLights from "../../resources/cube/lights.jpg";
 
 export default class Projects extends Component {
   render() {
     return (
       <Layout>
         <div className="ProjectContainer">
-          <div className="column">
-            <PreviewCardSmall sourceImage={cubeBrick} />
-            {/* this is where the project previews will go */}
-          </div>
+          <PreviewCardSmall sourceImage={cubeSide} />
+          <PreviewCardSmall sourceImage={cubeBrick} />
+          <PreviewCardSmall sourceImage={cubeLights} />
+          <PreviewCardSmall sourceImage={cubeLights} />
+          <PreviewCardSmall sourceImage={cubeSide} />
+          <PreviewCardSmall sourceImage={cubeBrick} />
+          <PreviewCardSmall sourceImage={cubeBrick} />
+          <PreviewCardSmall sourceImage={cubeLights} />
+          <PreviewCardSmall sourceImage={cubeSide} />
+          {/* this is where the project previews will go */}
         </div>
       </Layout>
     );
