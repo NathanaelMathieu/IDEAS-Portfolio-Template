274bcf3d14978bd01732a1a76e6fc5146fef8c5f
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-01-28 13:06:50 -0500

    project grid bugs are mostly squashed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,8 +2,16 @@ import "./Projects.scss";
 import React, { Component } from "react";
 import Layout from "../Layout.js";
 import { PreviewCard } from "../components";
-import cubeBrick from "../resources/cube/brick.jpg";
-import cubeLights from "../resources/cube/lights.jpg";
+import cubeBrick from "../resources/cube/brick_sd.jpg";
+import cubeHall from "../resources/cube/hall_sd.jpg";
+import cubeIso1 from "../resources/cube/iso1_sd.jpg";
+import cubeIso2 from "../resources/cube/iso2_sd.jpg";
+import cubeIso3 from "../resources/cube/iso3_sd.jpg";
+import cubeIso4 from "../resources/cube/iso4_sd.jpg";
+import cubeLights from "../resources/cube/lights_sd.jpg";
+import cubeSide from "../resources/cube/side1_sd.jpg";
+
+
 // Import cubeSide from "../../resources/cube/side.JPG";
 
 
@@ -12,17 +20,15 @@ export default class Projects extends Component {
     return (
       <Layout>
         <div className="ProjectContainer">
+          <PreviewCard sourceImage={cubeIso1} route={"projects/cube"}/>
+          <PreviewCard sourceImage={cubeLights} route={"projects/cube"}/>
+          <PreviewCard sourceImage={cubeIso2} route={"projects/cube"}/>
           <PreviewCard sourceImage={cubeBrick} route={"projects/cube"}/>
-          <PreviewCard sourceImage={cubeBrick} />
-          <PreviewCard sourceImage={cubeLights} />
-          <PreviewCard sourceImage={cubeBrick} />
-
-          <PreviewCard sourceImage={cubeLights} />
-          {/* <PreviewCardSmall sourceImage={cubeSide} />
-          <PreviewCardSmall sourceImage={cubeSide} /> */}
-          <PreviewCard sourceImage={cubeBrick} />
-          <PreviewCard sourceImage={cubeLights} />
-          {/* <PreviewCardSmall sourceImage={cubeSide} /> */}
+          <PreviewCard sourceImage={cubeIso3} />
+          <PreviewCard sourceImage={cubeHall} route={"projects/cube"}/>
+          <PreviewCard sourceImage={cubeIso4} />
+          <PreviewCard sourceImage={cubeLights} route={"projects/cube"}/>
+          <PreviewCard sourceImage={cubeSide} route={"projects/cube"}/>
           {/* This is where the project previews will go */}
         </div>
       </Layout>
