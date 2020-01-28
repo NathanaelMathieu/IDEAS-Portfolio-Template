fb0931fd3d0dbbe9db2f31a8ae9cd5883df35d67
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-01-28 16:07:38 -0500

    Add titles for project tabs

--- src/pages/projects/Cube.js
+++ src/pages/projects/Cube.js
@@ -5,6 +5,10 @@ import Layout from "Layout.js";
 // Import defaultImage from "resources/ImageNotFound.png";
 
 export default class Cube extends Component {
+  componentDidMount() {
+    document.title = "The Cube | Nathanael Mathieu";
+  }
+
   render() {
     return (
       <Layout>
