f13e04a294674425afc5200f12dee3d1d22636c8
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-04 14:58:43 -0500

    update photo imports, add professional photo

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,14 +2,14 @@ import "./Projects.scss";
 import React, { Component } from "react";
 import { ImageCard } from "../components";
 import Layout from "../Layout.js";
-import cubeBrick from "../resources/cube/brick_sd.jpg";
-import cubeHall from "../resources/cube/hall_sd.jpg";
-import cubeIso1 from "../resources/cube/iso1_sd.jpg";
-import cubeIso2 from "../resources/cube/iso2_sd.jpg";
-import cubeIso3 from "../resources/cube/iso3_sd.jpg";
-import cubeIso4 from "../resources/cube/iso4_sd.jpg";
-import cubeLights from "../resources/cube/lights_sd.jpg";
-import cubeSide from "../resources/cube/side1_sd.jpg";
+import cubeBrick from "../resources/projects/cube/brick_sd.jpg";
+import cubeHall from "../resources/projects/cube/hall_sd.jpg";
+import cubeIso1 from "../resources/projects/cube/iso1_sd.jpg";
+import cubeIso2 from "../resources/projects/cube/iso2_sd.jpg";
+import cubeIso3 from "../resources/projects/cube/iso3_sd.jpg";
+import cubeIso4 from "../resources/projects/cube/iso4_sd.jpg";
+import cubeLights from "../resources/projects/cube/lights_sd.jpg";
+import cubeSide from "../resources/projects/cube/side1_sd.jpg";
 
 export default class Projects extends Component {
   componentDidMount() {
