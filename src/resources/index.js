e9e7a4d992d8fccd162a7b6edb58507417ac92fb
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-06 12:11:07 -0500

    Add more placeholders images and change naming

---
+++ src/resources/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+export {
+  default as imageNotFound_4x6_high
+  } from "./ImageNotFound_4x6_high.png";
+export {
+  default as imageNotFound_4x6_med
+  } from "./ImageNotFound_4x6_med.png";
+export {
+  default as imageNotFound_4x6_low
+  } from "./ImageNotFound_4x6_low.png";
+export {
+  default as professionalPhoto
+  } from "./professionalPhoto.JPG";
