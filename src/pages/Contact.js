3f5d5fd6c64e22c257ceae04a99b95dca44e8f31
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-05 21:35:39 -0500

    Obey line length eslint rules

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -18,7 +18,15 @@ export default class Contact extends Component {
             <ImageCard image={professionalPhoto}/>
           </div>
           <div className="text_container">
-            <p>Scuppers gaff Yellow Jack bucko interloper hogshead Sink me trysail hands shrouds. Gibbet lateen sail belay measured fer yer chains spirits salmagundi Yellow Jack heave to Jolly Roger crimp. Belaying pin Sail ho yawl yard weigh anchor pillage black jack dance the hempen jig Davy Jones' Locker run a shot across the bow. Lad strike colors bilge rat red ensign Pieces of Eight chandler chase trysail capstan boom. Poop deck handsomely schooner grog Pirate Round Blimey gangway reef aye scallywag. </p>
+            <p>Scuppers gaff Yellow Jack bucko interloper hogshead Sink me
+               trysail hands shrouds. Gibbet lateen sail belay measured fer
+               yer chains spirits salmagundi Yellow Jack heave to Jolly Roger
+                crimp. Belaying pin Sail ho yawl yard weigh anchor pillage
+                 black jack dance the hempen jig Davy Jones' Locker run a
+                  shot across the bow. Lad strike colors bilge rat red ensign
+                   Pieces of Eight chandler chase trysail capstan boom. Poop
+                    deck handsomely schooner grog Pirate Round Blimey gangway
+                     reef aye scallywag. </p>
           </div>
           <div className="button_container">
             Button
