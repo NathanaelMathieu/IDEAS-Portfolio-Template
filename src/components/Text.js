8b6240a516578b3e7f7c9387b3af8d839d960c05
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-20 12:27:52 -0500

    Text Component more adaptable

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,18 +1,29 @@
+import "./Text.scss";
 import React from "react";
 import propTypes from "prop-types";
 
 function Text(props) {
-  return (
-    <pre className="Text">
+  if (props.preformatted) {
+    return <pre className={`Text ${props.style}`}>
       {props.text}
-    </pre>
-  );
+    </pre>;
+  }
+
+  return (
+    <div className={`Text ${props.style}`}>
+      {props.text}
+    </div>);
 }
 
 Text.propTypes = {
+  "preformatted": propTypes.bool,
+  "style": propTypes.string,
   "text": propTypes.string
 };
+
 Text.defaultProps = {
+  "preformatted": false,
+  "style": "",
   "text": ""
 };
 
