63ea80e2a33736b2e47581256c4048ab774ecc6b
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-23 22:58:07 -0500

    Cube Page exists, modeled after About

--- src/pages/projects/Cube.js
+++ src/pages/projects/Cube.js
@@ -1,7 +1,10 @@
-
+import "./Cube.scss";
 import React, { Component } from "react";
-import Layout from "Layout.js";
-// Import defaultImage from "resources/ImageNotFound.png";
+import { endText, mainText } from "./Text/cubeText.js";
+import { ImageCard } from "../../components";
+import Layout from "../../Layout.js";
+import cubeIso1 from "../../resources/projects/cube/iso1_sd.jpg";
+import cubeIso3 from "../../resources/projects/cube/iso3_sd.jpg";
 
 export default class Cube extends Component {
   componentDidMount() {
@@ -11,8 +14,23 @@ export default class Cube extends Component {
   render() {
     return (
       <Layout>
-        <div className="cubePage">
-          <p>Welcome to the Cube page!</p>
+        <div className="Cube">
+          <div className="main_picture">
+            <ImageCard alt="An isometric view of the Cube"
+              size="4x6" image={cubeIso1}
+              textEnabled text="An isometric view of the Cube"/>
+          </div>
+          <div className="main_text">
+          {mainText}
+          </div>
+          <div className="end_picture">
+            <ImageCard alt="An isometric view of the Cube"
+              textEnabled text="An isometric view of the Cube"
+              image={cubeIso3}/>
+          </div>
+          <div className="end_text">
+            {endText}
+          </div>
         </div>
       </Layout>
     );
---
+++ src/pages/projects/Text/cubeText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text } from "../../../components";
+
+export const mainText = <Text text=
+{
+`Chase schooner measured fer yer chains lateen sail wherry scuttle Sink me strike colors bilged on her anchor to go on account. Spirits gally boom Pieces of Eight smartly Sink me grog blossom keel parley Pirate Round. Trysail Buccaneer hearties nipperkin skysail hogshead tender plunder long boat salmagundi.
+Gibbet transom starboard Spanish Main measured fer yer chains Nelsons folly spike Sink me smartly Blimey. Parrel deadlights no prey, no pay brigantine fathom measured fer yer chains tackle red ensign mizzenmast piracy. Fluke grog lugger lanyard pinnace line bounty gally parrel holystone.
+Aye mizzenmast line rutters Letter of Marque stern sheet Gold Road execution dock bucko. No prey, no pay broadside hail-shot Jolly Roger fathom Gold Road chase shrouds heave down driver. Gun cutlass pirate yawl piracy Sail ho hands Nelsons folly ho coffer. `
+}
+/>;
+
+export const endText = <Text text=
+{
+`Ho lanyard grog blossom Jolly Roger dance the hempen jig coxswain chantey aye league barque hang the jib long boat quarter spirits crow's nest. Squiffy man-of-war mutiny brigantine marooned grog blossom reef sails smartly cackle fruit heave to Brethren of the Coast mizzenmast fire in the hole to go on account swab. Run a rig fluke avast quarterdeck swab hornswaggle lanyard clap of thunder Buccaneer bilge rat Arr hail-shot knave gangway American Main.
+Gangway skysail grog lee starboard topgallant gibbet Shiver me timbers sutler pink gabion spanker walk the plank Barbary Coast come about. Spyglass flogging hail-shot overhaul keelhaul aft knave loot lugsail scuppers broadside pirate mizzenmast boom quarter. Yo-ho-ho Cat o'nine tails ye fathom brig black jack Blimey case shot gibbet Jolly Roger bilge rat red ensign belay barkadeer rum. `
+}
+/>;
