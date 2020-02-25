bc10bcec0d3a73467e717ade93d9fa46e6940188
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-25 12:39:46 -0500

    IconButtons is a new component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header.js";
 export { default as ImageCard } from "./ImageCard.js";
 export { default as Text } from "./Text.js";
+export { default as IconButtons } from "./IconButtons.js";
