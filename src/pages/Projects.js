baf5a64a1fc3231bc702d25abd958ff271c6835d
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-26 13:23:22 -0500

    Remove name from sourcecode and rearrange cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,7 @@ import Layout from "../Layout.js";
 import cubeBrick from "../resources/projects/cube/brick_sd.jpg";
 import cubeHall from "../resources/projects/cube/hall_sd.jpg";
 import cubeIso1 from "../resources/projects/cube/iso1_sd.jpg";
+import cubeIso2 from "../resources/projects/cube/iso2_sd.jpg";
 import cubeIso3 from "../resources/projects/cube/iso3_sd.jpg";
 import cubeIso4 from "../resources/projects/cube/iso4_sd.jpg";
 import cubeLights from "../resources/projects/cube/lights_sd.jpg";
@@ -12,7 +13,7 @@ import cubeSide from "../resources/projects/cube/side1_sd.jpg";
 
 export default class Projects extends Component {
   componentDidMount() {
-    document.title = "Projects | Nathanael Mathieu";
+    document.title = "Projects | IDEAS Student";
   }
 
   render() {
@@ -20,18 +21,21 @@ export default class Projects extends Component {
       <Layout>
         <div className="ProjectContainer">
           <ImageCard image={cubeIso1} route="projects/cube"
-           textEnabled clickable alt="Click to visit my Cube project page"/>
+           textEnabled clickable alt="Click to visit my Cube project page"
+           text="Cube  |  Fall 2019"/>
           <ImageCard image={cubeLights} textEnabled
-           alt="Click to visit my Cube project page"/>
-          <ImageCard alt="Example placeholder image"/>
-          <ImageCard image={cubeBrick} alt="This is a placeholder image"
+           alt="Lights seen through the Cube"
+           text="Default text is 'This project is really cool!'"/>
+          <ImageCard image={cubeIso2} alt="This is a placeholder image"
            route="projects/cube" clickable/>
+          <ImageCard alt="This image is not specified"
+           text="This text won't be visible because textEnabled is not set"/>
           <ImageCard image={cubeIso3} alt="This is a placeholder image"/>
           <ImageCard image={cubeHall} alt="This is a placeholder image"/>
-          <ImageCard image={cubeIso4} />
-          <ImageCard image={cubeLights} alt="This is a placeholder image"/>
+          <ImageCard image={cubeIso4} alt="Alt text is required"/>
+          <ImageCard image={cubeBrick} alt="Example placeholder image"/>
           <ImageCard image={cubeSide} alt="This is a placeholder image"/>
-          {/* This is where the project previews will go */}
+          {/* Replace/add image cards here, each linking to a project */}
         </div>
       </Layout>
     );
