88ad99df95119b94ebdb601cc72745cfe6aef77f
Nathanael Mathieu <nmathieu@wesleyan.edu>
2020-02-28 10:28:21 -0500

    Updates

---
+++ precache-manifest.7a5f50f4f8affa0660c2c61748552315.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56114b072620cd735fbd32e005ff0d4a",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/index.html"
+  },
+  {
+    "revision": "629eb4382adb39d8ef76",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/css/main.0c11731a.chunk.css"
+  },
+  {
+    "revision": "e29ea61216696b258fc9",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/js/2.e53047c2.chunk.js"
+  },
+  {
+    "revision": "0723d8a793525d9889d8e42ae1c6e4dd",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/js/2.e53047c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "629eb4382adb39d8ef76",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/js/main.24c63a45.chunk.js"
+  },
+  {
+    "revision": "46a92fba88570d4805d3",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/js/runtime-main.4035a775.js"
+  },
+  {
+    "revision": "67087615042e5400d72e58a82fe4b967",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/ImageNotFound_4x6.67087615.png"
+  },
+  {
+    "revision": "487748ab45bd3459b82b7064f41bd54a",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/ImageNotFound_square.487748ab.png"
+  },
+  {
+    "revision": "f25f6aa75c1f6b93da253d96c2ad649f",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/ProfessionalPhoto.f25f6aa7.JPG"
+  },
+  {
+    "revision": "4d842d7f5d6f5ecc0ebec99c00af2406",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/brick.4d842d7f.jpg"
+  },
+  {
+    "revision": "5529c19cd5d531dfe2f16a4ba4ac06c6",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/hall.5529c19c.jpg"
+  },
+  {
+    "revision": "8096f5b6e115b13183c697a8cdf8ff74",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/iso1.8096f5b6.jpg"
+  },
+  {
+    "revision": "dd2c4b1f0f1560c4243257b58df418ba",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/iso2.dd2c4b1f.jpg"
+  },
+  {
+    "revision": "df609517e2c63ec1725fb7c942d02353",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/iso3.df609517.jpg"
+  },
+  {
+    "revision": "c4983d9395b5437459f67deb743941d9",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/iso4.c4983d93.jpg"
+  },
+  {
+    "revision": "b70637742c505f6b4768122d85796d9e",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/lights.b7063774.jpg"
+  },
+  {
+    "revision": "4af0f452ccb635ae31637346199baebd",
+    "url": "https://nathanaelmathieu.github.io/IDEAS-Portfolio-Template/static/media/side1.4af0f452.jpg"
+  }
+]);
